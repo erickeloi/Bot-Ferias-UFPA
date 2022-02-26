2ac1cd7775dc3f163f1faba53c76e3d26d36c372
Erick Eloi <erickx53@hotmail.com>
2022-02-26 13:30:47 -0300

    Melhorias e Adições no FeriasUFPA
    Correção de Bugs,
    
    Adição dos  Períodos Letivos de 2022, com inclusão dos Períodos INTENSIVOS e
    
    Reformatação do código fonte

--- FeriasUFPA.py
+++ FeriasUFPA.py
@@ -2,7 +2,6 @@ import os
 import tweepy
 from texto_do_tweet import *
 
-
 # Integração com o Twitter usando Tweepy:
 
 # 1. Informações da conta:
@@ -18,71 +17,38 @@ auth = tweepy.OAuthHandler(Consumer_Key, Consumer_Secret_Key)
 auth.set_access_token(Access_Token, Access_Secret_Token)
 api = tweepy.API(auth)
 
-
-# Criando um Log caso algo dê errado.
-def debug_text(local, error):
-    texto_do_debug = open('texto do debug.txt', 'w')
-    texto_do_debug.write(f"Deu erro no(a) {local}\n{error}\n")
-    texto_do_debug.close()
-
+mensagem_do_dia = texto_do_tweet()
 
 # 3. Função que posta a mensagem do dia
 def postar_mensagem_do_dia():
-    local = "Função Postar Mensagem do Dia"
     try:
-        mensagem_do_dia = texto_dos_bacanas()
         api.update_status(mensagem_do_dia)
-
-    except Exception as error:
-        debug_text(local, error)
+    except Exception as e:
+        api.update_status(f"Apareceu o seguinte erro: {e}\nPor favor avisar o Erick Herói !!!")
 
 
 # 4. Vendo se a mensagem já foi postada!
-meus_posts_recentes = api.user_timeline()
-
-
 def verificacao_de_tweet_duplicado():
-    local = "Função Verificação De Tweet Duplicado"
-    isPosted = False
-    try:
-        for post in meus_posts_recentes:
-            if post.text == texto_dos_bacanas():
-                isPosted = True
-        return isPosted
+    foiPostado = False
 
-    except Exception as error:
-        debug_text(local, error)
+    meus_posts_recentes = api.user_timeline()
+
+    for post in meus_posts_recentes:
+        if post.text == mensagem_do_dia:
+            foiPostado = True
+    return foiPostado
 
 
+# 5. Função que envia mensagem no direct (Opcional - Para testes)
 def enviar_mensagem_direta(nome_de_usuario: str, texto: str):
-    local = "Função Enviar_Mensagem_Direta"
-    try:
-        id_no_twitter = api.get_user(f"@{nome_de_usuario}").id
-        api.send_direct_message(str(id_no_twitter), texto)
+    id_no_twitter = api.get_user(f"@{nome_de_usuario}").id
+    api.send_direct_message(str(id_no_twitter), texto)
 
-    except Exception as error:
-        debug_text(local, error)
-
-
+# 6. Função Principal
 def main():
-    local = "Função Main"
-    try:
-        if verificacao_de_tweet_duplicado():
-            texto_da_mensagem = open('texto da mensagem.txt', 'w')
-            texto_da_mensagem.write(f"O tweet de hoje já foi postado anteriormente, Não esquente sua cabeça!\nA "
-                                    f"Mensagem do dia é {texto_da_mensagem}\n")
-            texto_da_mensagem.close()
-
-        elif not verificacao_de_tweet_duplicado():
-            postar_mensagem_do_dia()
-
-            texto_da_mensagem = open('texto da mensagem.txt', 'w')
-            texto_da_mensagem.write(f"Mermão, tudo NO ESQUEMA!!! O de hoje foi pago agora!!!\n"
-                                    f"A mensagem do dia é {texto_da_mensagem}\n")
-            texto_da_mensagem.close()
-
-    except Exception as error:
-        debug_text(local, error)
+    if not verificacao_de_tweet_duplicado():
+        postar_mensagem_do_dia()
 
 
-main()
+if __name__ == "__main__":
+    main()
--- texto_do_tweet.py
+++ texto_do_tweet.py
@@ -1,85 +1,127 @@
-from time import gmtime
+from datetime import date
 
-# Tempo em UTC:
-ano_atual = gmtime()[0]
-mes_atual = gmtime()[1]
-dia_atual = gmtime()[2]
+data_atual = date.today()
+ano_atual = data_atual.year
+mes_atual = data_atual.month
+dia_atual = data_atual.day
 
-# Caso a mensagem seja postada entre 21PM - 23:59PM (adaptar para UTC-3):
-if 0 <= gmtime().tm_hour <= 2:
-    dia_atual -= 1
+# Lógica das Datas:
+
+# Periodos Letivos - INTENSIVOS
+# 2022.1
+# yyyy-mm-dd
+# 2022-01-03 ( inicio do 2022.1 - INTENSIVO )
+# 2022-02-28 ( fim do 2022.1 - INTENSIVO )
+
+fim_pl20221 = date(2022, 2, 28)
+
+dias_faltando_fim_pl20221_aux = fim_pl20221 - data_atual
+
+dias_faltando_fim_pl20221 = dias_faltando_fim_pl20221_aux.days
+
+# Dias corridos = 56 dias
+# Dias letivos = 49 dias ( Sábado também conta )
+
+# 2022.3
+# 2022-07-01 ( inicio do 2022.3 - INTENSIVO )
+# 2022-08-31 ( fim do 2022.3 - INTENSIVO )
+
+# 61 dias corridos
+# 43 dias letivos
+
+fim_pl20223 = date(2022, 8, 31)
+
+dias_faltando_fim_pl20223_aux = fim_pl20223 - data_atual
+
+dias_faltando_fim_pl20223 = dias_faltando_fim_pl20223_aux.days
+# -------------------------------------------------
+
+# Periodos Letivos - EXTENSIVOS
+# 2022.2
+# 2022-03-14 ( inicio do 2022.2 - EXTENSIVO )
+# 2022-07-11 ( fim do 2022.2 - EXTENSIVO )
+
+# 119 dias corridos
+# 100 dias letivos
+
+fim_pl20222 = date(2022, 7, 11)
+
+dias_faltando_fim_pl20222_aux = fim_pl20222 - data_atual
+
+dias_faltando_fim_pl20222 = dias_faltando_fim_pl20222_aux.days
+
+# Periodos Letivos - EXTENSIVOS
+# 2022.4
+# 2022-08-22 ( inicio do 2022.4 - EXTENSIVO )
+# 2022-12-21 ( fim do 2022.4 - EXTENSIVO )
+
+# 121 dias corridos
+# 100 dias letivos
+
+fim_pl20224 = date(2022, 12, 21)
+
+dias_faltando_fim_pl20224_aux = fim_pl20224 - data_atual
+
+dias_faltando_fim_pl20224 = dias_faltando_fim_pl20224_aux.days
+
+
+# -------------------------------------------------
 
 # Lógica da Mensagem:
-tempo_decorrido_em_dias = ((mes_atual - 1) * 30 + dia_atual) + 2
-if ano_atual == 2022:
-    tempo_decorrido_em_dias += 365
-
-fim_periodo_2_ufpa_dias = 279
-
-# Periodo 2 UFPA
-# dias corridos de 01-01-2021 até 07/10/2021 = 279
-
-# 24/06/2021 a 07/10/2021
-# 105 dia(s) = 2° Período Letivo
-
-# 7 dias de férias até o próximo período letivo ( 3° )
-# dia 286 ele volta, 14/10/2021
-
-fim_periodo_3_ufpa_dias = 403
-
-# Periodo 3 UFPA
-# dias corridos de 01-01-2021 até 08/02/2022 = 403
-
-# 14/10/2021 a 08/02/2022
-# 117 dia(s) = 3° Período Letivo
-
-# Passou de 365, pois termina em 2022.
 
 
-def texto_dos_bacanas():
-    periodo = 2
+def texto_do_tweet():
+    periodo_letivo = str()
+    texto_final = ''
 
     def texto_padrao_dias(dias_que_faltam, periodo_atual):
-        return f"Faltam {dias_que_faltam} dias para as Férias (Término do {periodo_atual}° P.L.)"
+        return f"Faltam {dias_que_faltam} dias para as Férias (Término do P.L. {periodo_atual})\n"
 
     def texto_padrao_dia(periodo_atual):
-        return f"Falta 1 dia para as Férias ( Término do {periodo_atual}° P.L.)"
+        return f"Falta 1 dia para as Férias (Término do P.L. {periodo_atual})\n"
 
-    def texto_glorioso():
-        return "Chegou o Glorioso Dia Meus Bacanos!!!"
+    def texto_glorioso(periodo_atual):
+        return f"Chegou o Glorioso Dia Meus Bacanos !!! (Férias - Acabou o P.L. {periodo_atual})\n"
 
-    try:
-        quantos_dias_faltam = fim_periodo_2_ufpa_dias - tempo_decorrido_em_dias
+    if ano_atual == 2022 and 1 <= mes_atual <= 2:
+        periodo_letivo = '2022.1 - INTENSIVO'
 
-        if quantos_dias_faltam > 1:
-            return texto_padrao_dias(quantos_dias_faltam, periodo)
+        if dias_faltando_fim_pl20221 > 1:
+            texto_final += texto_padrao_dias(dias_faltando_fim_pl20221, periodo_letivo)
+        elif dias_faltando_fim_pl20221 == 1:
+            texto_final += texto_padrao_dia(dias_faltando_fim_pl20221)
+        elif dias_faltando_fim_pl20221 == 0:
+            texto_glorioso(periodo_letivo)
 
-        elif quantos_dias_faltam == 1:
-            return texto_padrao_dia(periodo)
+    elif ano_atual == 2022 and 3 <= mes_atual <= 7:
+        periodo_letivo = '2022.2 - EXTENSIVO'
 
-        elif quantos_dias_faltam == 0:
-            return texto_glorioso()
+        if dias_faltando_fim_pl20222 > 1:
+            texto_final += texto_padrao_dias(dias_faltando_fim_pl20222, periodo_letivo)
+        elif dias_faltando_fim_pl20222 == 1:
+            texto_final += texto_padrao_dia(dias_faltando_fim_pl20222)
+        elif dias_faltando_fim_pl20222 == 0:
+            texto_glorioso(periodo_letivo)
 
-        # Ele vai passar 7 dias desativado (Férias).
-        # E voltar 14/10/2021, começo do 3° Periodo Letivo
+    if ano_atual == 2022 and 7 <= mes_atual <= 8:
+        periodo_letivo = '2022.3 - INTENSIVO'
 
-        if tempo_decorrido_em_dias >= 286:
-            
-            quantos_dias_faltam = fim_periodo_3_ufpa_dias - tempo_decorrido_em_dias
-            periodo = 3
+        if dias_faltando_fim_pl20223 > 1:
+            texto_final += texto_padrao_dias(dias_faltando_fim_pl20223, periodo_letivo)
+        elif dias_faltando_fim_pl20223 == 1:
+            texto_final += texto_padrao_dia(dias_faltando_fim_pl20223)
+        elif dias_faltando_fim_pl20223 == 0:
+            texto_glorioso(periodo_letivo)
 
-            if quantos_dias_faltam > 1:
-                return texto_padrao_dias(quantos_dias_faltam, periodo)
+    if ano_atual == 2022 and 8 <= mes_atual <= 12:
+        periodo_letivo = '2022.4 - EXTENSIVO'
 
-            elif quantos_dias_faltam == 1:
-                return texto_padrao_dia(periodo)
+        if dias_faltando_fim_pl20224 > 1:
+            texto_final += texto_padrao_dias(dias_faltando_fim_pl20224, periodo_letivo)
+        elif dias_faltando_fim_pl20224 == 1:
+            texto_final += texto_padrao_dia(dias_faltando_fim_pl20224)
+        elif dias_faltando_fim_pl20224 == 0:
+            texto_glorioso(periodo_letivo)
 
-            elif quantos_dias_faltam == 0:
-                return texto_glorioso()
+    return texto_final
 
-    except Exception as error:
-        print("Deu erro\n", error)
-        return "Deu Erro na Mensagem dos bacanas!!! Chame o Erick Herói Imediatamente!!!"
-        
-    
-print(texto_dos_bacanas())
